aee952db9b7e5600139f2275107e038d307788f0
Jan Pochyla <jpochyla@gmail.com>
2014-03-26 14:16:58 +0100

    Fix error message on failed plugin config

--- app/scripts/controllers/error.js
+++ app/scripts/controllers/error.js
@@ -2,7 +2,7 @@
 
 angular.module('errorApp')
   .controller('ErrorCtrl', function (error, trezorApi, $scope) {
-  	$scope.error = error;
-  	$scope.installed = error.installed;
-  	$scope.installers = trezorApi.installers();
+    $scope.error = error;
+    $scope.installed = error.installed !== false;
+    $scope.installers = trezorApi.installers();
   });
