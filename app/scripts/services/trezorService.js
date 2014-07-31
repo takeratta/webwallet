fa18fb66b75c9113d4198582cb7d388cd0424d57
Jan Pochyla <jpochyla@gmail.com>
2014-07-31 18:36:42 +0200

    Firmware check is part of firmware modal now

--- app/scripts/services/trezorService.js
+++ app/scripts/services/trezorService.js
@@ -306,7 +306,7 @@ angular.module('webwalletApp')
         device: dev,
         update: function () {
           updateFirmware(scope, firmware);
-        },
+        }
       });
 
       modal = $modal.open({
@@ -336,32 +336,32 @@ angular.module('webwalletApp')
     //
 
     function updateFirmware(scope, firmware) {
+      var deregister;
+
+      scope.firmware = firmware;
       scope.state = 'update-downloading';
+
       firmwareService.download(firmware)
         .then(function (data) {
-          $rootScope.$on('modal.button.show', modalShown);
+          deregister = $rootScope.$on('device.button', promptButton);
           scope.state = 'update-flashing';
           return scope.device.flash(data);
         })
         .then(
           function () {
-            $rootScope.$off('modal.button.show', modalShown);
             scope.state = 'update-success';
+            deregister();
           },
           function (err) {
-            $rootScope.$off('modal.button.show', modalShown);
             scope.state = 'update-error';
             scope.error = err.message;
+            deregister();
           }
         );
 
-      function modalShown(event, code) {
+      function promptButton(event, dev, code) {
         if (code === 'ButtonRequest_FirmwareCheck')
-          injectFirmwareInfo(event.targetScope);
-      }
-
-      function injectFirmwareInfo(scope) {
-        scope.firmware = firmware;
+          scope.state = 'update-check';
       }
     }
 
