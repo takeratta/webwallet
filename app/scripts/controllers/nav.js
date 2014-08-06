a5275e1ca00157cbf2535bce6f640393f8927546
Jan Pochyla <jpochyla@gmail.com>
2014-08-06 17:35:00 +0200

    Keep active tab when switching accounts, even across devices
    Needed for comfortable switching of devices/accounts after URI handler activation.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -18,6 +18,12 @@ angular.module('webwalletApp')
           flash.error(err.message || 'Failed to add account.');
         }
       );
+    };
 
+    $scope.accountLink = function (dev, acc) {
+      var link = '#/device/' + dev.id + '/account/' + acc.id;
+      if ($scope.isActive('/receive$')) link += '/receive';
+      if ($scope.isActive('/send$')) link += '/send';
+      return link;
     };
   });
