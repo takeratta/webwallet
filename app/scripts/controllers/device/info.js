45663250c47d5e81eddb5f786363803b20bf2ac8
Jan Pochyla <jpochyla@gmail.com>
2014-09-24 13:36:56 +0200

    device: Show qr for account xpubs, on hover

---
+++ app/scripts/controllers/device/info.js
@@ -0,0 +1,11 @@
+angular.module('webwalletApp')
+  .controller('DeviceInfoCtrl', function (flash, $scope, $location) {
+    'use strict';
+
+    $scope.highlightedXpub = null;
+
+    $scope.highlightXpub = function (xpub) {
+      $scope.highlightedXpub = xpub;
+    };
+
+  });
