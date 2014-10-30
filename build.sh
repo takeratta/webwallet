38eb727d29690d61df8a354d6c49b6fe322e9090
Jan Pochyla <jpochyla@gmail.com>
2014-10-30 18:07:27 +0100

    bower: Fix -F

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ cd app/data
 cd ../../
 
 rm -rf app/bower_components
-bower install -F || $(npm bin)/bower install
+bower install -F || $(npm bin)/bower install -F
 #npm install
 
 grunt build || $(npm bin)/grunt build
