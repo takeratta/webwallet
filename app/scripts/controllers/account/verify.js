377f916fc72b7fd7c73ef6be4b458fdd429a9c7c
Jakub Valenta <valenta.jakub@gmail.com>
2014-11-13 15:23:16 +0100

    trezorService.js, verify.js: Removed, unused

--- app/scripts/controllers/account/verify.js
+++
@@ -1,9 +0,0 @@
-/*global angular*/
-
-angular.module('webwalletApp').controller('AccountVerifyCtrl', function (
-    flash, storage, utils, config, trezorService,
-    $filter, $scope, $rootScope, $routeParams) {
-
-    'use strict';
-
-});
